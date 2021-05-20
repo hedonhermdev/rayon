ddc0aaf3a74229cc24bf4eb70e578b455061c2f3
Tirth Jain <jaintirth24@gmail.com>
2021-05-20 15:42:49 +0530

    Fix example

--- examples/reverse_string.rs
+++ examples/reverse_string.rs
@@ -1,9 +1,9 @@
 use rayon::prelude::*;
 
 fn main() {
-    let powers: String = "abcdefg".par_chars()
+    let reversed: String = "abcdefg".par_chars()
         .map(|x| x.to_string())
         .rev_reduce(String::new, |mut a, b| {a.push_str(&b); a});
 
-    println!("{:?}", powers);
+    assert_eq!(reversed, "gfedcba".to_owned())
 }
