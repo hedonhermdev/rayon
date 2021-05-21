7843cdd963a8f8658a43190f6dd8663398195a1a
Tirth Jain <jaintirth24@gmail.com>
2021-05-21 11:49:58 +0530

    Add example

---
+++ examples/scan_ex.rs
@@ -0,0 +1,16 @@
+use rayon::prelude::*;
+
+fn main() {
+    rayon::ThreadPoolBuilder::new().num_threads(1).build_global().unwrap();
+
+    let nums: Vec<String> = "abcdefghijklmnopqrstuvwxyzabcdefghi".chars().map(|x| x.to_string()).collect();
+
+    let psums: Vec<String> = nums.par_iter()
+        .scan(String::new , |acc, x| {
+            acc.push_str(x);
+            
+            acc.clone()
+        }).collect();
+
+    println!("{:?}", psums);
+}
